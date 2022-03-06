8f4370391b806a72ff0253b11672c6313977c023
Mankianer <Marvin.woelk@web.de>
2022-03-06 16:29:16 +0100

    add game1 scoreboard

--- src/app/game1/scoreboard/scoreboard.component.ts
+++ src/app/game1/scoreboard/scoreboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GameKeeperService} from "../../game-keeper.service";
 
 @Component({
   selector: 'app-scoreboard',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public gameKeeper: GameKeeperService) { }
 
   ngOnInit(): void {
   }

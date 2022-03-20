26b4cc17bb57aaaa0944e3d3f100cfef122533df
Mankianer <Marvin.woelk@web.de>
2022-03-20 22:26:42 +0100

    update transition-screen look

--- src/app/transition-screen/transition-screen.component.ts
+++ src/app/transition-screen/transition-screen.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GameKeeperService} from "../game-keeper.service";
 
-export interface Score {
-  name: string;
-  score: number;
-  text: string;
+export class Score {
+  rank = 1;
+
+  constructor(public name: string, public score: number, public text: string) {
+  }
 }
 
 @Component({
@@ -15,29 +16,39 @@ export interface Score {
 export class TransitionScreenComponent implements OnInit {
 
 
-  constructor(public gameKeeper: GameKeeperService) { }
+  constructor(public gameKeeper: GameKeeperService) {
+  }
 
   ngOnInit(): void {
   }
 
   getScoreList(): Score[] {
-    return [this.getRed(), this.getBlue(), this.getGreen(), this.getPurple()].sort((a,b) => b.score - a.score);
+    let scores = [this.getRed(), this.getBlue(), this.getGreen(), this.getPurple()].sort((a, b) => b.score - a.score);
+    let offset = 0;
+    for (let i = 1; i < scores.length; i++) {
+      if (scores[i - 1].score == scores[i].score) {
+        offset++
+      }
+      scores[i].rank = (i + 1) - offset;
+
+    }
+    return scores;
   }
 
-  getRed(): Score{
-    return {name: "red", score: this.gameKeeper.score_red, text: "Team Rot"}
+  getRed(): Score {
+    return new Score("red", this.gameKeeper.score_red, "Team Rot");
   }
 
-  getBlue(): Score{
-    return {name: "blue", score: this.gameKeeper.score_blue, text: "Team Blau"}
+  getBlue(): Score {
+    return new Score("blue", this.gameKeeper.score_blue, "Team Blau");
   }
 
-  getGreen(): Score{
-    return {name: "green", score: this.gameKeeper.score_green, text: "Team Grün"}
+  getGreen(): Score {
+    return new Score("green", this.gameKeeper.score_green, "Team Grün");
   }
 
-  getPurple(): Score{
-    return {name: "purple", score: this.gameKeeper.score_purple, text: "Team Lila"}
+  getPurple(): Score {
+    return new Score("purple", this.gameKeeper.score_purple, "Team Lila");
   }
 
 }

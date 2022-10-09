c90cfa7e4fc065a9f52261e14701f3d467d4a010
Mankianer <mankianer@gmail.com>
2022-10-09 21:08:41 +0200

    add Undo to transition-screen

--- src/app/transition-screen/transition-screen.component.ts
+++ src/app/transition-screen/transition-screen.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {GameKeeperService} from "../game-keeper.service";
+import {ShortcutInput} from "ng-keyboard-shortcuts";
+import {UndoService} from "../undo.service";
 
 export class Score {
   rank = 1;
@@ -13,13 +15,29 @@ export class Score {
   templateUrl: './transition-screen.component.html',
   styleUrls: ['./transition-screen.component.css']
 })
-export class TransitionScreenComponent implements OnInit {
+export class TransitionScreenComponent implements OnInit, AfterViewInit {
 
+  shortcuts: ShortcutInput[] = [];
 
-  constructor(public gameKeeper: GameKeeperService) {
+  constructor(public gameKeeper: GameKeeperService, private undoService: UndoService) {
   }
 
   ngOnInit(): void {
+  }
+
+  undo() {
+    this.undoService.undo("transition-screen");
+  }
+
+  ngAfterViewInit(): void {
+    this.shortcuts.push({
+      key: ["ctrl + z"],
+      label: "Undo",
+      description: "Strg + Z",
+      command: this.undo.bind(this),
+      preventDefault: true
+    });
+    this.undoService.createSavepoint("transition-screen");
   }
 
   getScoreList(): Score[] {
